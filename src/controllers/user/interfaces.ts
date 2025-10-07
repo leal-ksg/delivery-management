11638561115307b5be7024d4ca0b62686630dc34
Kauã Leal <kauagsilvaleal@gmail.com>
2025-10-06 21:54:18 -0300

    refactor: change user role to follow prisma typing

--- src/controllers/user/interfaces.ts
+++ src/controllers/user/interfaces.ts
@@ -4,9 +4,9 @@ import type { User } from "../../models/user.js";
 
 export interface IUserRepository {
   findAll(): Promise<Result<User[]>>;
-  findById(id: string): Promise<Result<User>>;
+  findById(id: string): Promise<Result<User | null>>;
   create(user: Omit<User, "id">): Promise<Result<User>>;
-  update(id: string, user: Omit<User, "id">): Promise<Result<User>>;
+  update(id: string, user: Partial<User>): Promise<Result<User>>;
   delete(id: string): Promise<Result<void>>;
 }
 
