e7bddc8f4b2cff0d8e9d30fcef5d7fe98aa6b7c8
Kauã Leal <kauagsilvaleal@gmail.com>
2025-11-16 19:47:14 -0300

    feat: add order respository and order service extra validations

---
+++ src/repositories/postgres/order-repository.ts
@@ -0,0 +1,60 @@
+import { Order, PrismaClient } from "../../../generated/prisma";
+import { IOrderRepository } from "../../controllers/order/interfaces";
+import { parseDatabaseErrorMessage } from "../../core/parse-database-error-message";
+import { Result } from "../../core/result";
+import { prisma } from "../../database/prisma";
+import { CreateOrderDTO } from "../../models/order";
+
+export class OrderRepository implements IOrderRepository {
+  async findAll(): Promise<Result<Order[]>> {
+    try {
+      const orders = await prisma.order.findMany();
+
+      return { ok: true, body: orders };
+    } catch (err) {
+      return { ok: false, error: parseDatabaseErrorMessage(err, "Pedido") };
+    }
+  }
+
+  async findById(id: number): Promise<Result<Order | null>> {
+    try {
+      const order = await prisma.order.findUnique({ where: { id } });
+
+      return { ok: true, body: order };
+    } catch (err) {
+      return { ok: false, error: parseDatabaseErrorMessage(err, "Pedido") };
+    }
+  }
+
+  async create(
+    newOrder: CreateOrderDTO,
+    transaction: PrismaClient
+  ): Promise<Result<Order>> {
+    try {
+      const { products: _products, ...order } = newOrder;
+
+      const createdOrder = await transaction.order.create({ data: order });
+
+      return { ok: true, body: createdOrder };
+    } catch (err) {
+      return { ok: false, error: parseDatabaseErrorMessage(err, "Pedido") };
+    }
+  }
+
+  async update(
+    id: number,
+    order: Partial<Order>,
+    transaction: PrismaClient
+  ): Promise<Result<Order>> {
+    try {
+      const updatedOrder = await transaction.order.update({
+        where: { id },
+        data: order,
+      });
+
+      return { ok: true, body: updatedOrder };
+    } catch (err) {
+      return { ok: false, error: parseDatabaseErrorMessage(err, "Pedido") };
+    }
+  }
+}
--- src/services/order-service.ts
+++ src/services/order-service.ts
@@ -1,27 +1,35 @@
 import { Order } from "../../generated/prisma";
 import { ICustomerRepository } from "../controllers/customer/interfaces";
 import {
+  IOrderProductRepository,
   IOrderRepository,
   IOrderService,
 } from "../controllers/order/interfaces";
 import { IProductRepository } from "../controllers/product/interfaces";
+import { IStockRepository } from "../controllers/stock/interfaces";
 import { IUserRepository } from "../controllers/user/interfaces";
+import { parseDatabaseErrorMessage } from "../core/parse-database-error-message";
 import { Result } from "../core/result";
-import { CreateOrderDTO, UpdateOrderDTO } from "../models/order";
+import { prisma } from "../database/prisma";
+import { CreateOrderDTO, OrderProduct, UpdateOrderDTO } from "../models/order";
+
+type ValidationResult =
+  | { succeed: true; message: null }
+  | { succeed: false; message: string };
 
 export class OrderService implements IOrderService {
   constructor(
     private readonly userRepo: IUserRepository,
     private readonly orderRepo: IOrderRepository,
+    private readonly orderProductRepo: IOrderProductRepository,
     private readonly productRepo: IProductRepository,
-    private readonly customerRepo: ICustomerRepository
-    // TODO: private readonly stockRepo: IStockRepository,
+    private readonly customerRepo: ICustomerRepository,
+    private readonly stockRepo: IStockRepository
   ) {}
 
-  async validate(order: CreateOrderDTO | UpdateOrderDTO): Promise<{
-    succeed: boolean;
-    message: string | null;
-  }> {
+  async validate(
+    order: CreateOrderDTO | UpdateOrderDTO
+  ): Promise<ValidationResult> {
     const userResult = await this.userRepo.findById(order.userId);
 
     if (!userResult.ok) return { succeed: false, message: userResult.error };
@@ -32,9 +40,7 @@ export class OrderService implements IOrderService {
         message: "Usuário não encontrado para finalizar o pedido",
       };
 
-    const customerResult = await this.customerRepo.findById(
-      order.customerId
-    );
+    const customerResult = await this.customerRepo.findById(order.customerId);
 
     if (!customerResult.ok)
       return { succeed: false, message: customerResult.error };
@@ -45,8 +51,10 @@ export class OrderService implements IOrderService {
         message: "Cliente não encontrado para finalizar o pedido",
       };
 
-    for (const product of order.products) {
-      const productResult = await this.productRepo.findById(product.productId);
+    for (const orderProduct of order.products) {
+      const productResult = await this.productRepo.findById(
+        orderProduct.productId
+      );
 
       if (!productResult.ok)
         return { succeed: false, message: productResult.error };
@@ -57,6 +65,18 @@ export class OrderService implements IOrderService {
           message:
             "Não foi possível encontrar um dos produtos para finalizar o pedido",
         };
+
+      const product = productResult.body;
+
+      const stockResult = await this.stockRepo.findById(product.id);
+
+      if (!stockResult.ok)
+        return { succeed: false, message: stockResult.error };
+
+      if (stockResult.body?.quantity < orderProduct.quantity) {
+        const message = `O produto ${product.name} não tem estoque suficiente para esse pedido`;
+        return { succeed: false, message };
+      }
     }
 
     return { succeed: true, message: null };
@@ -69,20 +89,53 @@ export class OrderService implements IOrderService {
       return { ok: false, error: validationResult.message! };
     }
 
-    const orderCreationResult = await this.orderRepo.create(newOrder)
+    try {
+      const creationResult = await prisma.$transaction(async (transaction) => {
+        const orderResult = await this.orderRepo.create(newOrder, transaction);
 
-    return orderCreationResult
+        if (!orderResult.ok)
+          throw new Error(
+            orderResult.error ?? "Não foi possível criar o pedido"
+          );
+
+        const orderProducts: OrderProduct[] = []
+
+        for(const {productId, quantity} of newOrder.products) {
+          orderProducts.push({productId, quantity, orderId: orderResult.body.id})
+
+          const stockResult = await this.stockRepo.decrease({productId, quantity}, transaction)
+
+          if (!stockResult.ok)
+          throw new Error(
+            stockResult.error ?? "Não foi possível atualizar o estoque de um produto"
+          );
+        }
+
+        const orderProductsResult = await this.orderProductRepo.createMany(orderProducts, transaction);
+
+        if (!orderProductsResult.ok)
+          throw new Error(
+            orderProductsResult.error ?? "Não foi possível salvar os produtos do pedido"
+          );
+
+        return orderResult;
+      });
+
+      return creationResult;
+    } catch (err) {
+      return { ok: false, error: parseDatabaseErrorMessage(err, "Pedido") };
+    }
   }
 
   async updateOrder(id: number, order: UpdateOrderDTO): Promise<Result<Order>> {
     const validationResult = await this.validate(order);
 
     if (!validationResult.succeed) {
-      return { ok: false, error: validationResult.message! };
+      return { ok: false, error: validationResult.message };
     }
 
-    const orderUpdateResult = await this.orderRepo.update(id, order)
+    const orderUpdateResult = await this.orderRepo.update(id, order);
 
-    return orderUpdateResult
+    return orderUpdateResult;
   }
 }
