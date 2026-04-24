4ce5f7d91b10e47bba0bd8d4c1ef888a6b9605ed
Kauã Leal <kauagsilvaleal@gmail.com>
2026-04-23 23:59:56 -0300

    chore: add api url env variable

--- frontend/lib/api.ts
+++ frontend/lib/api.ts
@@ -5,7 +5,7 @@ export type ApiResponse<T> =
   | { ok: false; error: string };
 
 const api = axios.create({
-  baseURL: "http://localhost:3001/api/v1",
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
   headers: {
     "Content-Type": "application/json",
   },
