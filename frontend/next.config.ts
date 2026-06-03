90e72a7c4263962946592b9fce3f22fcbfe1ffa3
Kauã Leal <kauagsilvaleal@gmail.com>
2026-06-02 23:05:32 -0300

    feat: add mobile table checkbox

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  allowedDevOrigins: ["192.168.1.65"],
+  allowedDevOrigins: ["192.168.1.63"],
   images: {
     qualities: [1, 20, 50, 75],
   },
